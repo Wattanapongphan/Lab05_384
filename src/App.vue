fa3ef47e973775b6e6c3d2854d7a36a51d7ea221
Wattanapongphan <wattanapongphan@gmail.com>
2023-09-07 19:01:26 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,148 @@
+<script setup>
+import { ref } from 'vue'
+
+const imgUrl = ref(new URL('./assets/default.png', import.meta.url).href)
+const imgUrl2 = ref(new URL('./assets/default.png', import.meta.url).href)
+const rd1 = ref(Math.floor(Math.random() * 4) + 1)
+const rd2 = ref(Math.floor(Math.random() * 4) + 1)
+const score1 = ref(0)
+const score2 = ref(0)
+
+const resetGame = () => {
+  score1.value = 0
+  score2.value = 0
+  imgUrl.value = new URL('./assets/default.png', import.meta.url).href
+  imgUrl2.value = new URL('./assets/default.png', import.meta.url).href
+}
+
+const randomizeImages = () => {
+  rd1.value = Math.floor(Math.random() * 4) + 1
+  rd2.value = Math.floor(Math.random() * 4) + 1
+
+  if (rd1.value === 1 && rd2.value === 2) {
+
+    score2.value += 1 
+  } else if (rd2.value === 2 && rd1.value === 1) {
+    score1.value += 1 
+  } 
+  
+  else if (rd1.value === 2 && rd2.value === 3) {
+    score2.value += 1 
+  } else if (rd1.value === 3 && rd2.value === 2) {
+    score1.value += 1 
+  }
+  
+  else if (rd1.value === 3 && rd2.value === 1) {
+    score2.value += 1 
+  } else if (rd2.value === 3 && rd1.value === 1) {
+    score1.value += 1 
+  }
+
+  
+  else if (rd1.value === 1 && rd2.value === 2) {
+    score2.value += 1 
+  } else if (rd2.value === 1 && rd1.value === 2) {
+    score1.value += 1 
+  }
+
+  if (rd1.value === 4) {
+    score1.value += 1 
+  }
+  if (rd2.value === 4) {
+    score2.value += 1 
+  }
+
+
+  if (rd1.value === 1) {
+    imgUrl.value = new URL('./assets/rock.png', import.meta.url).href
+  } else if (rd1.value === 2) {
+    imgUrl.value = new URL('./assets/paper.jpg', import.meta.url).href
+  } else if (rd1.value === 3) {
+    imgUrl.value = new URL('./assets/scissors.jpg', import.meta.url).href
+  } else {
+    imgUrl.value = new URL('./assets/heart.png', import.meta.url).href
+  }
+
+  if (rd2.value === 1) {
+    imgUrl2.value = new URL('./assets/rock.png', import.meta.url).href
+  } else if (rd2.value === 2) {
+    imgUrl2.value = new URL('./assets/paper.jpg', import.meta.url).href
+  } else if (rd2.value === 3) {
+    imgUrl2.value = new URL('./assets/scissors.jpg', import.meta.url).href
+  } else {
+    imgUrl2.value = new URL('./assets/heart.png', import.meta.url).href
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="image-container">
+      <img :src="imgUrl" class="game-image" />
+      <span class="score">คะแนน: {{ score1 }}</span>
+    </div>
+    <div class="image-container">
+      <img :src="imgUrl2" class="game-image" />
+      <span class="score">คะแนน: {{ score2 }}</span>
+    </div>
+    <div class="button">
+    <button @click="randomizeImages" class="random-button">เริ่มเกม</button>
+    <button @click="resetGame" class="reset-button">เริ่มใหม่</button>
+  </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.buttons {
+  display: flex;
+  gap: 20px;
+  margin-top: 20px;
+}
+
+.game-image {
+  max-width: 200px;
+  height: auto;
+  margin: 10px;
+}
+
+.score {
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.random-button {
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  font-size: 16px;
+  border-radius: 5px;
+  margin-top: 20px;
+}
+
+.reset-button {
+  background-color: #ff0004;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  font-size: 16px;
+  border-radius: 5px;
+  margin-top: 20px;
+  margin-left: 50px;
+}
+
+.random-button:hover {
+  background-color: #0056b3;
+}
+</style>
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { ref } from 'vue';
+import { reactive } from 'vue';
+
+defineProps({
+  msg: String,
+})
+
+const x = ref(0);
+const y = ref(0);
+const answer = ref(0);
+
+const count = ref(0);
+let name = ref('Wattanapongphan');
+const lightstatus = ref(false)
+
+const styleObject = reactive({
+  color: 'red',
+  fontSize: '13px'
+})
+
+function upsize(){
+  count.value++;
+  console.log(styleObject.fontSize.slice(0,-2));
+  styleObject.fontSize = `${20 + count.value}px`;
+}
+
+function turnon(){
+lightstatus.value = !lightstatus.value;
+}
+
+function add(){
+answer.value = x.value+y.value;
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <h2 :style="styleObject" >{{ name }}</h2>
+
+  <form>
+    ชื่อ <input type="text" v-model="name"><br>
+    บวกเลข
+    <input type="number" v-model="x">+<input type="number" v-model="y"><br>
+    <button @click="add()">คำตอบ {{ answer }}</button>
+    
+  </form>
+
+  <div class="card">
+    <button @click="turnon">on/off</button>{{ lightstatus }}
+
+    <button type="button"  @click="upsize">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
